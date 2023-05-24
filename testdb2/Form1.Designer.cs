ce9638258a9886a0fcd98377f066de1883872673
21522354 <21522354@gm.uit.edu.vn>
2023-05-24 19:38:05 +0700

    add

--- testdb2/Form1.Designer.cs
+++ testdb2/Form1.Designer.cs
@@ -28,49 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtMASP = new System.Windows.Forms.TextBox();
+            this.txtTENSP = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // dataGridView1
-            // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(21, 59);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(571, 343);
-            this.dataGridView1.TabIndex = 0;
-            // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(631, 75);
+            this.button1.Location = new System.Drawing.Point(473, 61);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(56, 19);
             this.button1.TabIndex = 1;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(31, 61);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(332, 275);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // txtMASP
+            // 
+            this.txtMASP.Location = new System.Drawing.Point(404, 114);
+            this.txtMASP.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtMASP.Multiline = true;
+            this.txtMASP.Name = "txtMASP";
+            this.txtMASP.Size = new System.Drawing.Size(118, 27);
+            this.txtMASP.TabIndex = 3;
+            // 
+            // txtTENSP
+            // 
+            this.txtTENSP.Location = new System.Drawing.Point(404, 171);
+            this.txtTENSP.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtTENSP.Multiline = true;
+            this.txtTENSP.Name = "txtTENSP";
+            this.txtTENSP.Size = new System.Drawing.Size(118, 27);
+            this.txtTENSP.TabIndex = 4;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(698, 463);
+            this.Controls.Add(this.txtTENSP);
+            this.Controls.Add(this.txtMASP);
             this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtMASP;
+        private System.Windows.Forms.TextBox txtTENSP;
     }
 }
 
--- testdb2/Form1.cs
+++ testdb2/Form1.cs
@@ -12,15 +12,39 @@ namespace testdb2
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
+            demodatabaseEntities db = new demodatabaseEntities();
+            dataGridView1.DataSource = db.SANPHAMs.ToList();
+
+            txtMASP.DataBindings.Add(new Binding("Text", dataGridView1.DataSource, "MASP"));
+            txtTENSP.DataBindings.Add(new Binding("Text", dataGridView1.DataSource, "TENSP"));
+
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             demodatabaseEntities db = new demodatabaseEntities();
-            dataGridView1.DataSource = db.HOADONs.ToList();
+
+            var querry1 = from a in db.SANPHAMs
+                          join b in db.CTHDs on a.MASP equals b.MASP
+                          where a.NUOCSX == "Trung Quoc"
+                          select a;
+
+            var querry2 = from a in db.HOADONs
+                          join b in db.CTHDs
+                          on a.SOHD equals b.SOHD
+                          where a.NGHD.Value.Year == 2006
+                          select new { MASP = b.MASP , NGHD = a.NGHD};
+
+
+            var querry3 = db.SANPHAMs.Find("TV02");
+
+            MessageBox.Show(querry3.MASP + " " + querry3.TENSP + " " + querry3.GIA); 
+
+            dataGridView1.DataSource = querry3;
         }
     }
 }
